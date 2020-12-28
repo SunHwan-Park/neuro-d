b11758854b12d954484384f06b8bcbc67e7202f7
SunHwan-Park <57381062+SunHwan-Park@users.noreply.github.com>
2020-12-29 07:18:37 +0900

    feature/import: Add import svg file function

--- neuro-d/src/components/NavBar.vue
+++ neuro-d/src/components/NavBar.vue
@@ -2,9 +2,8 @@
   <div class="nav-content">
     <img class="logo" src="../assets/logo.png" />
     <div>
-      <v-btn elevation="2">
-        IMPORT
-      </v-btn>
+      <v-btn @click="clickImportSVG">IMPORT</v-btn>
+      <input v-show="false" ref="inputUpload" type="file" accept=".svg" @change="importSVG">
       <v-btn class="ml-2" elevation="2">
         EXPORT
       </v-btn>
@@ -13,8 +12,45 @@
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex'
 export default {
-  name: "NavBar"
+  name: "NavBar",
+  data() {
+    return {
+      svgText: null
+    }
+  },
+  computed: {
+    ...mapState(["svgObjs"])
+  },
+  methods: {
+    ...mapActions(["importSVG"]),
+    clickImportSVG() {
+      if (this.svgObjs.length) {
+        if (!confirm("Do you want to clear the Drawings?")) {
+          return;
+        }
+      }
+      this.$refs.inputUpload.click();
+    },
+    importSVG(event) {
+      const newFile = event.target.files[0];
+      const reader = new FileReader()
+      let result
+      reader.onload = function() {
+        result = reader.result;
+      }
+      reader.readAsText(newFile);
+      setTimeout(() => {
+        let canvas = document.querySelector(".inner-canvas");
+        canvas.innerHTML = result;
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(result, "image/svg+xml");
+        canvas.style.width = doc.children[0].width.animVal.value;
+        canvas.style.height = doc.children[0].height.animVal.value;
+      }, 500);
+    }
+  }
 }
 </script>
 
