fddf74b6b1776bac1bdcae15b5c0a7385161ed11
SunHwan-Park <57381062+SunHwan-Park@users.noreply.github.com>
2020-12-28 20:42:38 +0900

    feature/toolbox: Add toolbox elements & selectTool function

--- neuro-d/src/store/index.js
+++ neuro-d/src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    selectedTool: "hand"
   },
   mutations: {
+    SET_SELECTED_TOOL(state, tool) {
+      state.selectedTool = tool;
+    }
   },
   actions: {
+    selectTool({ commit }, tool) {
+      commit("SET_SELECTED_TOOL", tool);
+    }
   },
   modules: {
   }
